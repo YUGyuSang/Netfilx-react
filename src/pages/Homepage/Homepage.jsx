a0be72f584eaa587a7f09251b0da48789378e80f
gyusang <monet000414@naver.com>
2024-05-22 17:55:40 +0900

    fix:api호출 성공

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
 
+// 1.배너 만들기 => popular 아이템을 들고와서 첫 번째 영화를 보여주자
+// 2.popular movie
+// 3.top rated movie
+// 4.upcomming movie
 const Homepage = () => {
   return (
     <div>
-        Homepage
+        <Banner/>
     </div>
   )
 }
