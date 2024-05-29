b12ac4597e1ebc7e9e324535e4578ab3fc895e7a
gyusang <monet000414@naver.com>
2024-05-29 14:57:15 +0900

    fix:인기순,날짜순,별점순

--- src/pages/Movies/Moviepage.jsx
+++ src/pages/Movies/Moviepage.jsx
@@ -5,6 +5,10 @@ import { Alert, Col, Container, Row, Spinner } from 'react-bootstrap';
 import MovieCard from '../../common/MovieCard/MovieCard';
 import ReactPaginate from 'react-paginate';
 import './Moviepage.style.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 // nav바에 클릭해서 온경우 => pupㅕlarMovie 보여준다.
 // 키워드를 입력해서 온경우 => 키워드와 관련된 영화들을 보여준다.
@@ -12,11 +16,31 @@ import './Moviepage.style.css';
 //page state 만들기
 //페이지네이션 클릭할때마다 page 바꿔주기
 //page 값이 바뀔 때 마다 usesearchMovie에 page까지 넣어서 fetch
+
+const sortMovies = (movies, sort) => {
+  if (!movies) return [];
+  switch (sort) {
+    case 'popularity':
+      return [...movies].sort((a, b) => b.popularity - a.popularity);
+    case 'release_date':
+      return [...movies].sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+    case 'vote_average':
+      return [...movies].sort((a, b) => b.vote_average - a.vote_average);
+    default:
+      return movies;
+  }
+};
+
 const Moviepage = () => {
   const[query,setQuery] = useSearchParams();
   const[page,setPage] = useState(1);
+  const [sort, setSort] = useState('popularity.desc'); // 기본 정렬 옵션
   const keyword = query.get("q");
   
+  const handleSortChange = (sortOption) => {
+    setSort(sortOption);
+    setPage(1); // 정렬 옵션이 변경될 때 페이지를 1로 리셋
+  };
 
   const {data,isLoading,isError,error} = useSearchMovieQuery({keyword,page});
   const handlePageClick=({selected})=>{
@@ -37,14 +61,38 @@ const Moviepage = () => {
   if(isError){
     return <Alert variant='danger'>{error.message}</Alert>
   }
+
+  const sortedMovies = sortMovies(data?.results, sort);
   return (
     <Container>
       <Row>
-        <Col lg={4} xs={12}>{" "}필터{" "}</Col>
+        <Col lg={4} xs={12}>
+    <Navbar variant="dark" bg="dark" expand="lg" style={{width:'250px'}}>
+      <Container fluid>
+        <Navbar.Brand href="#home">정렬</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-dark-example" />
+        <Navbar.Collapse id="navbar-dark-example">
+          <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="고르기"
+              menuVariant="dark"
+              onSelect={handleSortChange}
+            >
+                    <NavDropdown.Item eventKey="popularity">인기 순</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="release_date">날짜 순</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="vote_average">별점 순</NavDropdown.Item>
+            <NavDropdown.Divider />
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </Col>
         
         <Col lg={8} xs={12}>
         <Row>
-          {data?.results.map((movie,index)=> (
+          {sortedMovies.map((movie,index)=> (
           <Col key={index} lg={4} xs={12}>
             <MovieCard movie={movie}/>
           </Col>
